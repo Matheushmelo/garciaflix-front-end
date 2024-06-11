e10b73db8e85189abdc0b1e459888e8a2a942e74
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-06-11 04:48:41 -0300

    Criação do header da página Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Head from "next/head";
-import styles from "./styles/HomeNoAuth.module.scss"
+import styles from "../styles/HomeNoAuth.module.scss"
+import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
 
 const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
-        <main></main>
+        <title>Garciaflix</title>
       </Head>
+      <body>
+        <main>
+          <HeaderNoAuth></HeaderNoAuth>
+        </main>
+      </body>
     </>
   )
 }
