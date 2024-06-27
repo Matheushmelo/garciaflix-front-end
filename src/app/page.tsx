fd4158e20749e9745ac732345715f4015d679004
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-06-27 05:13:34 -0300

    Adicionando animações com o AOS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import styles from "../styles/HomeNoAuth.module.scss"
+import styles from "../styles/HomeNoAuth.module.scss";
 import HeaderNoAuth from "@/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "@/components/homeNoAuth/presentationSection";
 import CardsSection from "@/components/homeNoAuth/cardsSection";
 import SlideSection from "@/components/homeNoAuth/slideSection";
 import getNewestCourses, {CourseType} from "@/services/courseService";
 import Footer from "@/components/common/footer";
+import AOSinit from "@/components/homeNoAuth/aosInit";
 
 export default async function HomeNoAuth() {
   const courses: CourseType[] = await getNewestCourses();
@@ -16,14 +17,19 @@ export default async function HomeNoAuth() {
         <title>Garciaflix</title>
       </Head>
       <main>
-        <div className={styles.sectionBackground}>
+        <div className={styles.sectionBackground} data-aos="fade-zoom-in" data-aos-duration="1600">
           <HeaderNoAuth />
           <PresentationSection />
         </div>
-        <CardsSection />
-        <SlideSection newestCourses={courses}/>
+        <div data-aos="fade-right" data-aos-duration="1200">
+          <CardsSection />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={courses}/>
+        </div>
         <Footer />
        </main>
+      <AOSinit /> 
     </>
   )
 }
---
+++ src/components/homeNoAuth/aosInit/index.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css"
+
+export default function AOSinit() {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <></>
+  )
+}
