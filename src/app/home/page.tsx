cf3edf117f2082f03b4b843b83815b47c5a42406
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-07-07 14:53:11 -0300

    Criando seção de lançamentos

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import FeaturedSection from "@/components/homeAuth/featuredSection"
+import NewestCategory from "@/components/homeAuth/newestCategory"
 import Head from "next/head"
 
 const Home = function() {
@@ -12,6 +13,7 @@ const Home = function() {
       </Head>
       <main>
         <FeaturedSection />
+        <NewestCategory />
       </main>
     </>
   )
--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -10,10 +10,10 @@ interface props {
 const SlideComponent = function ({ course }: props) {
   let slideCount = 0
 
-  if (course.length > 4) {
+  if (course?.length > 4) {
     slideCount = 4
   } else if (course) {
-    slideCount = course.length
+    slideCount = course?.length
   }
 
   return (
@@ -26,20 +26,20 @@ const SlideComponent = function ({ course }: props) {
             perMove: 1,
             width: slideCount * 300,
             pagination: false,
-            arrows: course.length > 4 ? true : false,
-            drag: course.length > 4 ? true : false,
+            arrows: course?.length > 4 ? true : false,
+            drag: course?.length > 4 ? true : false,
             breakpoints: {
               1200: {
                 perPage: slideCount >= 2 ? 2 : 1,
                 width: slideCount >= 2 ? 600 : 300,
-                arrows: course.length > 2 ? true : false,
-                drag: course.length > 2 ? true : false,
+                arrows: course?.length > 2 ? true : false,
+                drag: course?.length > 2 ? true : false,
               },
               600: {
                 perPage: 1,
                 width: 300,
-                arrows: course.length > 1 ? true : false,
-                drag: course.length > 1 ? true : false,
+                arrows: course?.length > 1 ? true : false,
+                drag: course?.length > 1 ? true : false,
               },
               300: {
                 width: 250,
