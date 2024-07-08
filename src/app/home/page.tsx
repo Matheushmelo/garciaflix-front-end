48f55e7668f42c9fda006e952f1e28c0b740e0d1
Matheus Henrique <104454290+Matheushmelo@users.noreply.github.com>
2024-07-08 09:37:55 -0300

    Criando seção de lista de categorias de cursos

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,6 +3,7 @@
 import FavoriteCategory from "@/components/homeAuth/favoriteCategory"
 import FeaturedCategory from "@/components/homeAuth/featuredCategory"
 import FeaturedSection from "@/components/homeAuth/featuredSection"
+import ListCategories from "@/components/homeAuth/listCategories"
 import NewestCategory from "@/components/homeAuth/newestCategory"
 import Head from "next/head"
 
@@ -10,7 +11,7 @@ const Home = function() {
   return (
     <>
       <Head>
-        <title>Garciaflix - Login</title>
+        <title>Garciaflix - Home</title>
         <link rel="shortcut icon" href="/favicon.png" type="image/x-icon" />
       </Head>
       <main>
@@ -18,6 +19,7 @@ const Home = function() {
         <NewestCategory />
         <FavoriteCategory />
         <FeaturedCategory />
+        <ListCategories />
       </main>
     </>
   )
---
+++ src/components/homeAuth/listCategories/index.tsx
@@ -0,0 +1,21 @@
+import categoriesService, { CategoryType } from "@/services/categoriesService"
+import useSWR from "swr"
+import ListCategoriesSlide from "../listCategoriesSlide"
+
+const ListCategories = function() {
+  const {data, error} = useSWR("/listCategories", categoriesService.getCategories)
+
+  if(error) return error
+
+  if(!data) return (<><p>Loading...</p></>)
+
+  return (
+    <>
+      {data.Category?.map((category: CategoryType) => (
+        <ListCategoriesSlide key={category.id} categoryId={category.id} categoryName={category.name}/>
+      ))}
+    </>
+  )
+}
+
+export default ListCategories
